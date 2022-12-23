f61e7d5e00ef7f3fe7300b8788215aca4eb1950d
Rajeev <rajeevkr8917@gmail.com>
2022-12-23 06:37:52 +0530

    adding of post via api

--- src/server.js
+++ src/server.js
@@ -7,12 +7,14 @@ const { usersRoute } = require('./routes/users')
 const { postsRoute } = require('./routes/posts')
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/users', usersRoute)
 app.use('/api/posts', postsRoute)
 app.use('/', express.static(__dirname + '/public'))
 
-db.sync({ force: true })
+db.sync()
     .then(() => {
         app.listen(5555, () => {
             console.log("Server started on http://localhost:5555")
